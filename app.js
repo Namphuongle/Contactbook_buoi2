41d9eacec51fac36e7085b342b5838519c3da861
phuongNam <lephuongnam11062001@gmail.com>
2022-09-23 16:53:50 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const cors  = require("cors");
 
-const contactsRoute = require("./app/routes/contact.route")
+const contactsRouter = require("./app/routes/contact.route");
+
+const ApiError = require("./app/api-error");
 
 const app = express();
 
  
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts", contactsRoute);
+app.use("/api/contacts", contactsRouter);
+
+//handler 404 response
+app.use((req, res, next) => {
+
+    return next(new ApiError(404, "Resourse not found"));
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
 
 app.get("/",(req, res) => {
     res.json({message: "Welcome to contact book application."});
