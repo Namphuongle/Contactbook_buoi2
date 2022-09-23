841ccf4a16389cb9cc05a254080773baf50ceddb
phuongNam <lephuongnam11062001@gmail.com>
2022-09-23 16:47:01 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors  = require("cors");
 
+const contactsRoute = require("./app/routes/contact.route")
+
 const app = express();
 
  
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRoute);
 
 app.get("/",(req, res) => {
     res.json({message: "Welcome to contact book application."});
